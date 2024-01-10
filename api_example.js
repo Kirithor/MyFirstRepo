6e054151221b528d4beca0a6f95c68a4a63fe112
Kirithor <simenbjerke0@gmail.com>
2024-01-10 08:47:11 +0100

    Learning about JSON functions

--- api_example.js
+++ api_example.js
@@ -4,3 +4,15 @@ $.ajax ({
         console.log(response)
     }
 })
+
+$.ajax ({
+    url: 'https://api.github.com/repos/Kirithor/MyFirstRepo',
+    processData: false,
+    success: function(response) {
+        console.log(response)
+        const serialized = JSON.stringify(response);
+        console.log(serialized);
+        const deserialized = JSON.parse(serialized);
+        console.log(deserialized);
+    }
+})
